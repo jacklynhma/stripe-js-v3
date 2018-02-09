bbdf549d7b4eed73ca2ee965a938922a360734d3
jacklynhma <jacklynhma@gmail.com>
2018-02-09 13:27:28 -0500

    refund

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,22 @@
 class UsersController < ApplicationController
 
+
+  def index
+    @users = User.all
+  end
+
   def create
-  
+
+  end
+
+  def refund
+    byebug
+    @charge_id = params[:charge_id]
+    # if we are keeping record of charges we should undo it
+    Stripe::Refund.create(
+      charge: @charge_id
+    )
+
+    redirect_to users_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :user 
+  resources :users do
+    member do
+      post :refund
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
